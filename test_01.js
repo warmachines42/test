62ef2a03fdb8d1c549f00a0bfdcb0a1df281ff2f
warmachines42 <warmachines42@gmail.com>
2026-09-20 17:03:41 +0900

    aa

--- test_01.js
+++ test_01.js
@@ -1,5 +1,6 @@
 var count = Math.floor(Math.random() * 10) + 1 ; 
 for (let i = 0; i < count ; i++) {
     document.write( 'nice to meet you!', i ); 
-    document.write( '<br/>' ); 
+    document.write( '<br/>' );
+    console.log(i);
 }
